03ce62732ef3c7c29a28f0c9ae48f760789e7259
Philippe Proulx <eeppeliteloop@gmail.com>
2023-11-02 10:21:40 -0400

    [tmp]
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

---
+++ tests/json/test.cpp
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+/*
+ * Copyright 2022 Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
+ */
+
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#include <side/trace.h>
+
+int main()
+{
+}
